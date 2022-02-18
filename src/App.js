2021693ea460b9325a9b23680526e9d0e5629c0d
vishal palla <vishalpalla@vishals-MacBook-Air.local>
2022-02-18 18:39:34 +0530

    mapbox-gl

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
-import './App.css';
-import ReactMapGL, { Marker } from 'react-map-gl'
-import { useState } from 'react';
-import  data from './data/sample.json';
-import { IconButton } from '@material-ui/core';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-function App() {
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useEffect, useRef, useState } from 'react';
 
-  const [viewport, setViewport] = useState({
-    latitude: 38.578433,
-    longitude: -121.499908,
-    width: '100vw',
-    height: '100vh',
-    zoom:10
+mapboxgl.accessToken = "pk.eyJ1IjoidmlzaGFsa3VtYXIzOTIiLCJhIjoiY2t6czlibHZ1NDlxcTJ5bnJqdnBua2tlZiJ9.udJvqdKsdqEMt7dNcWdnvQ";
+
+export default function App() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
   });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
-    <div >
-      <ReactMapGL 
-      initialViewState={viewport}
-      mapboxAccessToken="pk.eyJ1IjoidmlzaGFsa3VtYXIzOTIiLCJhIjoiY2t6czlibHZ1NDlxcTJ5bnJqdnBua2tlZiJ9.udJvqdKsdqEMt7dNcWdnvQ"
-      mapStyle="mapbox://styles/vishalkumar392/ckzs9ooyu009o15npb90uf13m"
-      style={{width:1200, height: 600}}
-      onDrag={viewport => {setViewport(viewport)}}
-      >
-        {data.providers.map((provider,index)=> {
-          console.log("lat: ",provider.providerAddress[0].lat);
-          return (
-            <Marker 
-              key={index}
-              latitude={provider.providerAddress[0].lat}
-              longitude={provider.providerAddress[0].lon}
-            >
-               {/* <IconButton>
-                <LocationOnIcon/>
-              </IconButton>  */}
-              <button
-              className="marker-btn"
-              
-            >
-              <img src="/skateboarding.svg" alt="Skate Park Icon" />
-            </button>
-            </Marker>
-          )
-        })}
-      </ReactMapGL>
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
-}
 
-export default App;
+}
